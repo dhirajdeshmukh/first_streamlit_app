6589a556884d23153bbe5f8950cc185bf6d0508c
dhirajdeshmukh <dhirajbdeshmukh@gmail.com>
2023-01-06 04:45:28 -0500

    Update streamlit_app.py
    Added a new header for Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@
 import streamlit
 streamlit.title('Hello World, This is my Parents\' new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avacado Toast')
 streamlit.text('Parata')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
